caad55c4c1017647cabaa1854a5067852b8bf64a
Anh-Than <124812659+Anh-Than@users.noreply.github.com>
2023-06-02 01:08:45 +0700

    finish raw

--- Assets/Script/Game/Enemy.cs
+++ Assets/Script/Game/Enemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     //Health,AttackPower,MoveSpeed
-    public int health,attackPower;
+    public int health, attackPower;
     public float moveSpeed;
 
     public Animator animator;
@@ -14,15 +14,15 @@ public class Enemy : MonoBehaviour
 
     void Update()
     {
-        if(!detectedTower)
+        if (!detectedTower)
         {
             Move();
-        }        
+        }
     }
 
     IEnumerator Attack()
     {
-        animator.Play("Attack",0,0);
+        animator.Play("Attack", 0, 0);
         //Wait attackInterval 
         yield return new WaitForSeconds(attackInterval);
         //Attack Again
@@ -33,17 +33,20 @@ public class Enemy : MonoBehaviour
     void Move()
     {
         animator.Play("Move");
-        transform.Translate(-transform.right*moveSpeed*Time.deltaTime);
+        transform.Translate(-transform.right * moveSpeed * Time.deltaTime);
     }
 
     public void InflictDamage()
     {
-        bool towerDied = detectedTower.LoseHealth(attackPower);
-
-        if (towerDied)
+        if (detectedTower != null)
         {
-            detectedTower = null;
-            StopCoroutine(attackOrder);
+            bool towerDied = detectedTower.LoseHealth(attackPower);
+
+            if (towerDied)
+            {
+                detectedTower = null;
+                StopCoroutine(attackOrder);
+            }
         }
     }
 
@@ -55,18 +58,18 @@ public class Enemy : MonoBehaviour
         //Blink Red animation
         StartCoroutine(BlinkRed());
         //Check if health is zero => destroy enemy
-        if(health<=0)
+        if (health <= 0)
             Destroy(gameObject);
     }
 
     IEnumerator BlinkRed()
     {
         //Change the spriterendere color to red
-        GetComponent<SpriteRenderer>().color=Color.red;
+        GetComponent<SpriteRenderer>().color = Color.red;
         //Wait for really small amount of time 
         yield return new WaitForSeconds(0.2f);
         //Revert to default color
-        GetComponent<SpriteRenderer>().color=Color.white;
+        GetComponent<SpriteRenderer>().color = Color.white;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -74,10 +77,10 @@ public class Enemy : MonoBehaviour
         if (detectedTower)
             return;
 
-        if(collision.tag == "Tower")
+        if (collision.tag == "Tower")
         {
             detectedTower = collision.GetComponent<Tower>();
             attackOrder = StartCoroutine(Attack());
-        }   
-    }    
+        }
+    }
 }
