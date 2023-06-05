d412a4fb4859797f769ef245cc886c341b4362b4
Anh-Than <124812659+Anh-Than@users.noreply.github.com>
2023-06-06 03:48:36 +0700

    score appear in end game screen

--- Assets/GameManagerScrip.cs
+++ Assets/GameManagerScrip.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManagerScrip : MonoBehaviour
 {
     public GameObject gameOverUI;
     public static GameManagerScrip instance;
+    [SerializeField] Text curScore;
     // Start is called before the first frame update
     void Awake() { instance = this; }
     void Start()
@@ -25,6 +27,7 @@ public class GameManagerScrip : MonoBehaviour
     public void gameOver()
     {
         gameOverUI.SetActive(true);
+        curScore.text = GameManager.instance.score.score.ToString();
         Time.timeScale = 0f;
     }
 }
